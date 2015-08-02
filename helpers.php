e6be2945caa178edf75a63cf6f06432cc831f97e
Eric Dowell <eric@d0we11.com>
2015-08-02 14:38:43 -0500

    Fixing pathing to vendor and themes folder

--- helpers.php
+++ helpers.php
@@ -7,11 +7,11 @@ if( !function_exists( 'themes_base_path' ) ) {
      */
     function themes_base_path()
     {
-        if( is_dir( realpath( __DIR__ . '/../../vendor' ) ) ) {
-            return realpath( __DIR__ . '/../../themes' );
+        if( is_dir( realpath( __DIR__ . '/../../../vendor' ) ) ) {
+            return realpath( __DIR__ . '/../../../themes' );
         }
-        if( is_dir( realpath( __DIR__ . '/../../themes' ) ) ) {
-            return realpath( __DIR__ . '/../../themes' );
+        if( is_dir( realpath( __DIR__ . '/../../../themes' ) ) ) {
+            return realpath( __DIR__ . '/../../../themes' );
         }
         if( getenv( 'APP_ENV' ) === 'testing' ) {
             return realpath( __DIR__ . '/tests/themes' );
